1841b90378a3603c204dff398197d93f04e643da
connor1337 <60065396+connor1337@users.noreply.github.com>
2021-05-28 19:30:34 -0400

    added exit codes

--- MI_000148_MW-Check.ps1
+++ MI_000148_MW-Check.ps1
@@ -1,10 +1,16 @@
-﻿$sh = @('28332bdbfaeb8333dad5ada3c10819a1a015db9106d5e8a74beaaf03797511aa','d7982ffe09f947e5b4237c9477af73a034114af03968e3c4ce462a029f072a5a')
+$sh = @('28332bdbfaeb8333dad5ada3c10819a1a015db9106d5e8a74beaaf03797511aa','d7982ffe09f947e5b4237c9477af73a034114af03968e3c4ce462a029f072a5a')
 $sf = @('audio.exe','frpc.exe','frps.exe')
 $ff = @()
 
 foreach($i in (gci -Include $sf -Path "C:\" -Recurse -ErrorAction SilentlyContinue).FullName)
 {
-    if ((Get-FileHash -Algorithm SHA256 $i).Hash -in $sh){$ff+=$i}
+    if ((Get-FileHash -Algorithm SHA256 $i).Hash -in $sh) { $ff+=$i }
 }
 
-if ( $ff.count -gt 0 ) { Write-Host "FOUND HASHES!! " $ff  } else { Write-Host "NO HASHES!" }
+if ( $ff.count -gt 0 )
+{
+    #hashes found!!
+    $ff
+    exit 5
+} 
+else {exit 0}
